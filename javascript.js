3549c229d7f355cede040dee1bae048c227c28da
Yuvarani25 <145268723+Yuvarani25@users.noreply.github.com>
2024-07-22 17:27:26 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function wrong1(){
+   document.querySelector("#a").style.backgroundColor="red";
+}
+function wrong2(){
+    document.querySelector("#b").style.backgroundColor="red";
+ }
+ function wrong3(){
+    document.querySelector("#c").style.backgroundColor="red";
+ }
+function correct(){
+   document.querySelector("#d").style.backgroundColor="green"
+}
